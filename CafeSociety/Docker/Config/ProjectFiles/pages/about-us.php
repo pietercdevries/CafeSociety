a1b1116ceb2c5f6a07b0b819eae1ffa9e68def4d
Pieter de Vries <pieterandcamryn@gmail.com>
2020-11-18 16:58:16 -0700

    Added about page

--- CafeSociety/Docker/Config/ProjectFiles/pages/about-us.php
+++ CafeSociety/Docker/Config/ProjectFiles/pages/about-us.php
@@ -1,3 +1,69 @@
 <?php require 'layout/header.php' ?>
 
+<!-- page css classes -->
+<link rel="stylesheet" href="/public/css/about-us.css">
+
+<!-- section -->
+<div class="section bg-blue" style="min-height: 100vh;">
+    <!-- main content -->
+    <div class="main-content">
+        <h1 class="content-title first-title-push tan" style="margin-top: 150px;">ABOUT US</h1>
+        <h3 class="red">PACIFIC NORTHWEST&#44; BORN AND BRED&#33;</h3>
+        <div class="about-section">
+            <div class="avatar-container">
+                <img class="avatar" src="/public/img/ryan.png" alt="Ryan" title="Creative Director Ryan"/>
+            </div>
+            <div class="description-container">
+                <h2 class="tan">Ryan</h2>
+                <h3 class="red">Creative Director&#47;Motion Designer</h3>
+                <p class="tan">
+                    After acquiring a degree in Visual Effects & Motion Graphics from The Art Institute of Portland in 2013&#44; Ryan spent a few years working in the animation field for an in&#8210;house marketing firm&#44; and eventually launched his own freelance business in 2015 before returning home to Grants Pass&#44; OR&#46;  He then served as Lead Motion Designer for Dutch Bros Coffee from 2016 &#8210; 2020&#44; doing a variety of promotional&#44; social media marketing&#44; and live&#8210;events content with an amazing team of other creatives&#46; Ryan also has an interest in film&#8210;making and writing&#46;
+                    <br>
+                    <br>
+                    He has produced a handful short films&#44; and hopes to eventually make feature films!
+                </p>
+            </div>
+        </div>
+        <div class="about-section">
+            <div class="avatar-container">
+                <img class="avatar" src="/public/img/tori.png" alt="Tori" title="Production Coordinator Tori"/>
+            </div>
+            <div class="description-container">
+                <h2 class="tan">Tori</h2>
+                <h3 class="red">Production Coordinator&#47;Writer</h3>
+                <p class="tan">
+                    In 2014&#44; Tori graduated &#34;suma cum laude&#34; from George Fox University in Newberg&#44; OR&#44; with a double major in Communications & Psychology and promptly landed a position as a production coordinator at Lanphier Associates Inc&#46; in Medford&#44;OR &#40;now called Paper & String&#41;  After a couple years&#44; she decided she wanted a taste of adventure and left Lanphier to start a new chapter of her life in Kona&#44; Hawaii&#44; where she served on staff for YWAM&#39;s University of the Nations program in 2016&#46; Later&#44; she returned home to Oregon&#44; where she worked for the City of Grants Pass as a Social Media Manager and Coordinator&#46;
+                </p>
+            </div>
+        </div>
+    </div>
+    <!--end main content -->
+</div>
+<!-- end section -->
+
+<!-- section -->
+<div class="section bg-blue">
+    <!-- main content -->
+    <div class="main-content">
+        <h1 class="content-title tan">OUR MISSION</h1>
+        <div class="divider bg-red"></div>
+        <p class="content-title tan">
+            Cafe Society is dedicated to producing the best video
+            solutions for your product or service.
+            <br>
+            <br>
+            Our goal is to create video content that pushes brands forward in
+            ground-breaking ways and promotes passionate collaboration.
+            <br>
+            <br>
+            We’re here to help you succeed in sharing your gift with the world.
+            <br>
+            <br>
+            Let’s create great things together.
+        </p>
+        <h3 class="red" style="margin-bottom: 50px;">Schedule a free 30-minute consultation today!</h3>
+    </div>
+    <!--end main content -->
+</div>
+<!-- end section -->
 <?php require 'layout/footer.php' ?>
